57b8d5254d39d195a91e82731453a4fe16d3baeb
natachabourg <natacha.bourg@univ-tln.fr>
2023-06-12 23:00:36 +0200

    Better gridding

--- Calibration_Camera/calibration_grid.m
+++ Calibration_Camera/calibration_grid.m
@@ -7,14 +7,13 @@ size_y = 14;
 
 grid_x = NaN*ones(size_x,size_y);
 grid_y = NaN*ones(size_x,size_y);
-
+%%
 path= '/home/nbourg/Bluebottle_Experiments/Calibration_Camera/';
-file =[path 'calib_anne_png.png']
+file =[path 'calib_anne_screen.png']
 
 I = imread(file);
-imshow(I)
-
-for i = 1:size_y
+imshow(rgb2ycbcr(I))
+for i = 7:size_y
     for j = 1:size_x
         [X,Y]= ginput(1)
         
@@ -23,49 +22,28 @@ for i = 1:size_y
 
     end
 end
+%%
 
-save('calib1st.mat','grid_x','grid_y')
+
+save('calib2nd.mat','grid_x','grid_y')
 
 %%
 
 
-dist_x = NaN*ones(14,21);
-dist_y = NaN*ones(14,21);
+dist_x = NaN*ones(21,14);
+dist_y = NaN*ones(21,14);
 
 
 for i=1:21
-    dist_x(:,i) = 0:20:(14-1)*20;
+    dist_x(i,:) = 0:20:(14-1)*20;
 end
 
 
 for i=1:14
-    dist_y(i,:) = 0:20:(21-1)*20;
+    dist_y(:,i) = 0:20:(21-1)*20;
 end
 
 
-%%
-
-load('calib1st.mat')
-
-gx = reshape(grid_x, [14,21]);
-gy = reshape(grid_y, [14,21]);
-
-
-scatter(gx,gy);
-
-gx(gy<99)=NaN;
-gy(gy<99)=NaN;
-%%
-
-path= '/home/nbourg/Bluebottle_Experiments/Calibration_Camera/';
-file =[path 'calib_anne_png.png']
-
-I = imread(file);
-
-imshow((I));
-hold on;
-
-pcolor(gx,gy,dist_y)
 
 %% test 
 
@@ -91,10 +69,12 @@ pcolor(gx,gy,dist_y)
 
 
 
-imshow((Ic));
+imshow((I));
 hold on;
 
-pcolor(gx,gy,dist_y)
+pcolor(grid_x,grid_y,dist_y)
+
+
 scatter(bb_points_x_left, bb_points_y_left,'r','filled')
 
 
