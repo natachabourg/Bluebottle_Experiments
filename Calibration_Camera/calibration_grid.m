89a3f29ff2c55ad0bf3efae8c5d25d8f205c8b49
natachabourg <natacha.bourg@univ-tln.fr>
2023-06-12 21:43:29 +0200

    progress on calibration

--- Calibration_Camera/calibration_grid.m
+++ Calibration_Camera/calibration_grid.m
@@ -2,7 +2,11 @@ clear all; close all;
 
 %%
 
-clear all
+size_x = 21;
+size_y = 14;
+
+grid_x = NaN*ones(size_x,size_y);
+grid_y = NaN*ones(size_x,size_y);
 
 path= '/home/nbourg/Bluebottle_Experiments/Calibration_Camera/';
 file =[path 'calib_anne_png.png']
@@ -10,8 +14,8 @@ file =[path 'calib_anne_png.png']
 I = imread(file);
 imshow(I)
 
-for i = 1:21
-    for j = 1:14
+for i = 1:size_y
+    for j = 1:size_x
         [X,Y]= ginput(1)
         
         grid_x(j,i) = X;
@@ -20,7 +24,78 @@ for i = 1:21
     end
 end
 
+save('calib1st.mat','grid_x','grid_y')
 
-imshow(rgb2gray(I));hold on;
-scatter(grid_x(1:10,:),grid_y(1:10,:),'red','filled')
+%%
+
+
+dist_x = NaN*ones(14,21);
+dist_y = NaN*ones(14,21);
+
+
+for i=1:21
+    dist_x(:,i) = 0:20:(14-1)*20;
+end
+
+
+for i=1:14
+    dist_y(i,:) = 0:20:(21-1)*20;
+end
+
+
+%%
+
+load('calib1st.mat')
+
+gx = reshape(grid_x, [14,21]);
+gy = reshape(grid_y, [14,21]);
+
+
+scatter(gx,gy);
+
+gx(gy<99)=NaN;
+gy(gy<99)=NaN;
+%%
+
+path= '/home/nbourg/Bluebottle_Experiments/Calibration_Camera/';
+file =[path 'calib_anne_png.png']
+
+I = imread(file);
+
+imshow((I));
+hold on;
+
+pcolor(gx,gy,dist_y)
+
+%% test 
+
+
+
+ for i = 1:5
+    
+    imshow(I)
+
+
+    [X,Y]= ginput(1)
+    
+    bb_points_x_left(i) = X;
+    bb_points_y_left(i) = Y
+    close all;
+
+
+    close all;
+ end
+
+
+%%
+
+
+
+imshow((Ic));
+hold on;
+
+pcolor(gx,gy,dist_y)
+scatter(bb_points_x_left, bb_points_y_left,'r','filled')
+
+
 
